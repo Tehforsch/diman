6ea0050e13b01a55db86b7535585bb63de76147e
tehforsch <mail@tonipeter.de>
2022-12-23 15:58:26 +0100

    add procedural macro to generate boilerplate Dimension code

---
+++ crates/derive_dimension/src/lib.rs
@@ -0,0 +1,146 @@
+use quote::quote;
+use syn::*;
+
+const ALLOWED_TYPES: &[&str] = &["i8", "i32", "i64"];
+
+#[proc_macro_attribute]
+pub fn diman_dimension(
+    _args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let methods_impl: proc_macro2::TokenStream = dimension_methods_impl(input.clone()).into();
+    let input: proc_macro2::TokenStream = input.into();
+    let output = quote! {
+        #input
+
+        #methods_impl
+    };
+    output.into()
+}
+
+pub(crate) fn dimension_methods_impl(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap();
+    let type_name = &ast.ident;
+    let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
+    let panic_unexpected_type = || panic!("Found unexpected field type while deriving diman_dimension methods.");
+    let mut field_names = vec![];
+    if let syn::Data::Struct(s) = ast.data {
+        if let syn::Fields::Named(fields) = s.fields {
+            for f in fields.named.iter() {
+                if let syn::Type::Path(ref type_name) = f.ty {
+                    if type_name.path.segments.len() != 1 {
+                        panic_unexpected_type();
+                    }
+                    else {
+                        let only_segment_in_path = &type_name.path.segments[0];
+                        let type_name = only_segment_in_path.ident.to_string();
+                        if !ALLOWED_TYPES.contains(&type_name.as_str()) {
+                            panic_unexpected_type();
+                        }
+                    }
+                }
+                else {
+                   panic_unexpected_type(); 
+                }
+                field_names.push(f.ident.as_ref().unwrap().clone());
+            }
+        }
+    }
+
+    let mul_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            #ident: self.#ident + other.#ident,
+        }
+    }).collect();
+
+    let div_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            #ident: self.#ident - other.#ident,
+        }
+    }).collect();
+
+    let inv_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            #ident: -self.#ident,
+        }
+    }).collect();
+
+    let powi_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            #ident: self.#ident * other,
+        }
+    }).collect();
+
+    let sqrt_safety_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            if self.#ident % 2 != 0 {
+                panic!("Cannot take square root of quantity with a dimension that is not divisible by 2 in all components.");
+            }
+        }
+    }).collect();
+
+    let sqrt_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            #ident: self.#ident / 2,
+        }
+    }).collect();
+
+    let cbrt_safety_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            if self.#ident % 3 != 0 {
+                panic!("Cannot take cubic root of quantity with a dimension that is not divisible by 3 in all components.");
+            }
+        }
+    }).collect();
+
+    let cbrt_gen: proc_macro2::TokenStream = field_names.iter().map(|ident| {
+        quote! {
+            #ident: self.#ident / 3,
+        }
+    }).collect();
+
+    let gen = quote! {
+        impl #impl_generics #type_name #type_generics #where_clause {
+            pub const fn dimension_mul(self, other: Self) -> Self {
+                Self {
+                    #mul_gen
+                }
+            }
+
+            pub const fn dimension_div(self, other: Self) -> Self {
+                Self {
+                    #div_gen
+                }
+            }
+
+            pub const fn dimension_inv(self) -> Self {
+                Self {
+                    #inv_gen
+                }
+            }
+
+            pub const fn dimension_powi(self, other: i32) -> Self {
+                Self {
+                    #powi_gen
+                }
+            }
+
+            pub const fn dimension_sqrt(self) -> Self {
+                #sqrt_safety_gen
+                Self {
+                    #sqrt_gen
+                }
+            }
+
+            pub const fn dimension_cbrt(self) -> Self {
+                #cbrt_safety_gen
+                Self {
+                    #cbrt_gen
+                }
+            }
+        }
+    };
+    gen.into()
+}
--- src/si/dimension.rs
+++ src/si/dimension.rs
@@ -1,9 +1,12 @@
+use derive_dimension::diman_dimension;
+
 const MASS_TO_SI: f64 = 1.0;
 const LENGTH_TO_SI: f64 = 1.0;
 const TIME_TO_SI: f64 = 1.0;
 const TEMPERATURE_TO_SI: f64 = 1.0;
 
 #[derive(PartialEq, Eq, Debug, Clone)]
+#[diman_dimension]
 pub struct Dimension {
     pub length: i32,
     pub time: i32,
@@ -19,78 +22,6 @@ pub(crate) const NONE: Dimension = Dimension {
 };
 
 impl Dimension {
-    pub const fn dimension_mul(self, rhs: Self) -> Self {
-        Self {
-            length: self.length + rhs.length,
-            mass: self.mass + rhs.mass,
-            time: self.time + rhs.time,
-            temperature: self.temperature + rhs.temperature,
-        }
-    }
-
-    pub const fn dimension_div(self, rhs: Self) -> Self {
-        Self {
-            length: self.length - rhs.length,
-            mass: self.mass - rhs.mass,
-            time: self.time - rhs.time,
-            temperature: self.temperature - rhs.temperature,
-        }
-    }
-
-    pub const fn dimension_powi(self, rhs: i32) -> Self {
-        Self {
-            length: self.length * rhs,
-            mass: self.mass * rhs,
-            time: self.time * rhs,
-            temperature: self.temperature * rhs,
-        }
-    }
-
-    pub const fn dimension_inv(self) -> Self {
-        Self {
-            length: -self.length,
-            mass: -self.mass,
-            time: -self.time,
-            temperature: -self.temperature,
-        }
-    }
-
-    pub const fn dimension_sqrt(self) -> Self {
-        if self.length % 2 != 0 {
-            panic!("Cannot take square root of quantity with a length dimension that is not divisible by 2");
-        } else if self.time % 2 != 0 {
-            panic!("Cannot take square root of quantity with a time dimension that is not divisible by 2");
-        } else if self.mass % 2 != 0 {
-            panic!("Cannot take square root of quantity with a mass dimension that is not divisible by 2");
-        } else if self.temperature % 2 != 0 {
-            panic!("Cannot take square root of quantity with a temperature dimension that is not divisible by 2");
-        }
-        Self {
-            length: self.length / 2,
-            mass: self.mass / 2,
-            time: self.time / 2,
-            temperature: self.temperature / 2,
-        }
-    }
-
-    pub const fn dimension_cbrt(self) -> Self {
-        if self.length % 3 != 0 {
-            panic!("Cannot take cube root of quantity with a length dimension that is not divisible by 3");
-        } else if self.time % 3 != 0 {
-            panic!("Cannot take cube root of quantity with a time dimension that is not divisible by 3");
-        } else if self.mass % 3 != 0 {
-            panic!("Cannot take cube root of quantity with a mass dimension that is not divisible by 3");
-        } else if self.temperature % 3 != 0 {
-            panic!("Cannot take cube root of quantity with a temperature dimension that is not divisible by 3");
-        }
-        Self {
-            length: self.length / 3,
-            mass: self.mass / 3,
-            time: self.time / 3,
-            temperature: self.temperature / 3,
-        }
-    }
-
     /// Get the base conversion factor of this dimension
     /// into SI units. As of now, this is always 1.0
     /// but will change if the base units are changed
