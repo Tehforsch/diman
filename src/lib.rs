14927bde5a22f39171a6ba8a75b5a711e409f74b
tehforsch <mail@tonipeter.de>
2023-05-23 00:36:48 +0200

    export macro

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,7 @@ mod rand;
 mod test_utils;
 
 pub use derive_dimension::diman_dimension;
+pub use unit_system::unit_system;
 pub use type_aliases::Product;
 pub use type_aliases::QProduct;
 pub use type_aliases::Quotient;
