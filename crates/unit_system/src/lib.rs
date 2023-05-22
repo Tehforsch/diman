33d8c541c9d636155323bd3725ce3fbad4b11a4b
tehforsch <mail@tonipeter.de>
2023-05-22 11:44:18 +0200

    move types into their own module

--- crates/unit_system/src/lib.rs
+++ crates/unit_system/src/lib.rs
@@ -1,11 +1,8 @@
-use proc_macro2::Span;
+mod types;
+
 use quote::{format_ident, quote};
-use syn::{
-    parse::{Parse, ParseBuffer, ParseStream},
-    punctuated::Punctuated,
-    spanned::Spanned,
-    *,
-};
+use syn::*;
+use types::{Defs, QuantityEntry};
 
 #[proc_macro]
 pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -13,11 +10,12 @@ pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     // Only temporary to make the transition less menacing
     let dimension_type = &defs.dimension_type;
+    let unit_names_type = &defs.unit_names_type;
     let unit_names_array_gen: proc_macro2::TokenStream = defs
         .quantities
         .iter()
         .flat_map(|quantity| {
-            let dimension = get_dimension_definition(&defs.dimension_type, &quantity);
+            let dimension = defs.get_dimension_definition(&quantity);
             quantity.units_def.units.iter().map(move |unit| {
                 let unit_symbol = unit.symbol.as_ref().unwrap();
                 let unit_factor = unit.factor;
@@ -29,7 +27,7 @@ pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
         .collect();
 
     let unit_names_array_gen = quote! {
-        pub const UNIT_NAMES: &[(#dimension_type, &str, f64)] = &[
+        pub const #unit_names_type: &[(#dimension_type, &str, f64)] = &[
             #unit_names_array_gen
         ];
     };
@@ -38,11 +36,12 @@ pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
         .quantities
         .iter()
         .map(|quantity| {
-            let dimension = get_dimension_definition(&defs.dimension_type, &quantity);
+            let dimension = defs.get_dimension_definition(&quantity);
             let quantity_type = &defs.quantity_type;
             let quantity_name = &quantity.name;
             let _vec2_ident = format_ident!("Vec2{}", quantity_name);
             let _vec3_ident = format_ident!("Vec2{}", quantity_name);
+            let _vec_ident = format_ident!("Vec{}", quantity_name);
             let quantity_def = quote! {
                 #[cfg(feature = "default-f64")]
                 pub type #quantity_name = #quantity_type::<f64, { #dimension }>;
@@ -53,12 +52,22 @@ pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
                     feature = "glam",
                     any(feature = "default-f32", feature = "default-f64")
                 ))]
-                pub type vec2_ident = #quantity_type<MVec2, { #dimension }>;
+                pub type #_vec2_ident = #quantity_type<MVec2, { #dimension }>;
                 #[cfg(all(
                     feature = "glam",
                     any(feature = "default-f32", feature = "default-f64")
                 ))]
-                pub type vec3_ident = #quantity_type<MVec3, { #dimension }>;
+                pub type #_vec3_ident = #quantity_type<MVec3, { #dimension }>;
+                #[cfg(all(
+                    feature = "glam",
+                    feature = "default-f32")
+                )]
+                pub type #_vec_ident = #quantity_type<MVec, { #dimension }>;
+                #[cfg(all(
+                    feature = "glam",
+                    feature = "default-f64")
+                )]
+                pub type #_vec_ident = #quantity_type<MVec, { #dimension }>;
             };
 
             let units_def = quantity
@@ -86,6 +95,32 @@ pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
                                 self.0 / #factor
                             }
                         }
+
+                        #[cfg(feature = "glam")]
+                        impl #quantity_type<::glam::Vec2, {#dimension}> {
+                            pub fn #unit_name(x: f32, y: f32) -> #quantity_type<::glam::Vec2, {#dimension}> {
+                                #quantity_type::<::glam::Vec2, {#dimension}>(::glam::Vec2::new(x, y) * #factor)
+                            }
+                        }
+                        #[cfg(feature = "glam")]
+                        impl #quantity_type<::glam::Vec3, {#dimension}> {
+                            pub fn #unit_name(x: f32, y: f32, z: f32) -> #quantity_type<::glam::Vec3, {#dimension}> {
+                                #quantity_type::<::glam::Vec3, {#dimension}>(::glam::Vec3::new(x, y, z) * #factor)
+                            }
+                        }
+                        #[cfg(feature = "glam")]
+                        impl #quantity_type<::glam::DVec2, {#dimension}> {
+                            pub fn #unit_name(x: f64, y: f64) -> #quantity_type<::glam::DVec2, {#dimension}> {
+                                #quantity_type::<::glam::DVec2, {#dimension}>(::glam::DVec2::new(x, y) * #factor)
+                            }
+                        }
+                        #[cfg(feature = "glam")]
+                        impl #quantity_type<::glam::DVec3, {#dimension}> {
+                            pub fn #unit_name(x: f64, y: f64, z: f64) -> #quantity_type<::glam::DVec3, {#dimension}> {
+                                #quantity_type::<::glam::DVec3, {#dimension}>(::glam::DVec3::new(x, y, z) * #factor)
+                            }
+                        }
+
                     }
                 })
                 .collect::<proc_macro2::TokenStream>();
@@ -99,360 +134,24 @@ pub fn unit_system_2(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
         .collect::<proc_macro2::TokenStream>().into()
 }
 
-fn get_dimension_definition(dimension_ty: &Type, q: &QuantityEntry) -> proc_macro2::TokenStream {
-    let field_updates: proc_macro2::TokenStream = q
-        .dimensions_def
-        .fields
-        .iter()
-        .map(|field| {
-            let ident = &field.ident;
-            let value = &field.value.val;
-            quote! { #ident: #value, }
-        })
-        .collect();
-    quote! {
-        #dimension_ty {
-            #field_updates
-            ..#dimension_ty::none()
-        }
-    }
-}
-
-struct Defs {
-    pub dimension_type: Type,
-    pub quantity_type: Type,
-    pub quantities: Vec<QuantityEntry>,
-}
-
-struct DimensionInt {
-    val: i32,
-}
-
-impl Parse for DimensionInt {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lit: Lit = input.parse()?;
-        let val = match lit {
-            Lit::Int(x) => x.base10_parse(),
-
-            _ => Err(Error::new(input.span(), "Expected int literal.")),
-        }?;
-        Ok(Self { val })
-    }
-}
-
-struct DimensionEntry {
-    ident: Ident,
-    value: DimensionInt,
-}
-
-impl Parse for DimensionEntry {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let ident: Ident = input.parse()?;
-        let _: Token![:] = input.parse()?;
-        let value: DimensionInt = input.parse()?;
-        Ok(Self { ident, value })
-    }
-}
-
-struct DimensionsEntry {
-    fields: Punctuated<DimensionEntry, Token![,]>,
-}
-
-impl Parse for DimensionsEntry {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let content;
-        let _: token::Brace = braced!(content in input);
-        let fields: Punctuated<DimensionEntry, Token![,]> =
-            content.parse_terminated(DimensionEntry::parse)?;
-        Ok(Self { fields })
-    }
-}
-
-struct Factor {
-    factor: f64,
-}
-
-impl Parse for Factor {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lit: Lit = input.parse()?;
-        let factor = match lit {
-            Lit::Int(x) => x.base10_parse(),
-
-            Lit::Float(x) => x.base10_parse(),
-            _ => Err(Error::new(input.span(), "Expected float literal.")),
-        }?;
-        Ok(Self { factor })
-    }
-}
-
-struct Symbol {
-    symbol: String,
-}
-
-impl Parse for Symbol {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lit: Lit = input.parse()?;
-        let symbol = match lit {
-            Lit::Str(x) => Ok(x.value()),
-            _ => Err(Error::new(input.span(), "Expected string literal.")),
-        }?;
-        Ok(Self { symbol })
-    }
-}
-
-struct Prefixes {
-    prefixes: Vec<Ident>,
-}
-
-impl Parse for Prefixes {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let content;
-        let _: token::Bracket = bracketed!(content in input);
-        let prefixes: Punctuated<Ident, Token![,]> = content.parse_terminated(Ident::parse)?;
-        Ok(Prefixes {
-            prefixes: prefixes.into_iter().collect(),
-        })
-    }
-}
-enum UnitDefinitionEntry {
-    Name(Ident),
-    Factor(Factor),
-    Symbol(Symbol),
-    Prefixes(Prefixes),
-}
-
-impl Parse for UnitDefinitionEntry {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let name: Ident = input.parse()?;
-        let _: Token![:] = input.parse()?;
-        match name.to_string().as_str() {
-            "name" => Ok(Self::Name(input.parse()?)),
-            "factor" => Ok(Self::Factor(input.parse()?)),
-            "symbol" => Ok(Self::Symbol(input.parse()?)),
-            "prefixes" => Ok(Self::Prefixes(input.parse()?)),
-            ident => Err(Error::new(
-                ident.span(),
-                format!("Unexpected identifier: {}", ident),
-            )),
-        }
-    }
-}
-
-struct UnitEntry {
-    name: Ident,
-    factor: f64,
-    symbol: Option<String>,
-    prefixes: Vec<Ident>,
-}
-
-impl Parse for UnitEntry {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let mut name: Option<Ident> = None;
-        let mut factor: Option<Factor> = None;
-        let mut symbol: Option<Symbol> = None;
-        let mut prefixes: Option<Prefixes> = None;
-        let content;
-        let _: token::Brace = braced!(content in input);
-        let entries: Punctuated<UnitDefinitionEntry, Token![,]> =
-            content.parse_terminated(UnitDefinitionEntry::parse)?;
-        for entry in entries.into_iter() {
-            match entry {
-                UnitDefinitionEntry::Name(def_name) => set_option_and_throw_error_if_is_some(
-                    &mut name,
-                    def_name,
-                    "name",
-                    input.span(),
-                )?,
-                UnitDefinitionEntry::Factor(def_factor) => set_option_and_throw_error_if_is_some(
-                    &mut factor,
-                    def_factor,
-                    "factor",
-                    input.span(),
-                )?,
-                UnitDefinitionEntry::Symbol(def_symbol) => set_option_and_throw_error_if_is_some(
-                    &mut symbol,
-                    def_symbol,
-                    "symbol",
-                    input.span(),
-                )?,
-                UnitDefinitionEntry::Prefixes(def_prefixes) => {
-                    set_option_and_throw_error_if_is_some(
-                        &mut prefixes,
-                        def_prefixes,
-                        "prefixes",
-                        input.span(),
-                    )?
-                }
+impl Defs {
+    fn get_dimension_definition(&self, q: &QuantityEntry) -> proc_macro2::TokenStream {
+        let dimension_type = &self.dimension_type;
+        let field_updates: proc_macro2::TokenStream = q
+            .dimensions_def
+            .fields
+            .iter()
+            .map(|field| {
+                let ident = &field.ident;
+                let value = &field.value.val;
+                quote! { #ident: #value, }
+            })
+            .collect();
+        quote! {
+            #dimension_type {
+                #field_updates
+                ..#dimension_type::none()
             }
         }
-        let name = name.unwrap();
-        let factor = factor.unwrap().factor;
-        let symbol = symbol.map(|symbol| symbol.symbol);
-        let prefixes = prefixes.map(|prefixes| prefixes.prefixes).unwrap_or(vec![]);
-        Ok(Self {
-            name,
-            factor,
-            symbol,
-            prefixes,
-        })
-    }
-}
-
-fn set_option_and_throw_error_if_is_some<T>(
-    item: &mut Option<T>,
-    new_item: T,
-    name: &str,
-    span: Span,
-) -> Result<()> {
-    if item.is_some() {
-        Err(Error::new(
-            span,
-            format!("More than one definition of {} given.", name),
-        ))
-    } else {
-        *item = Some(new_item);
-        Ok(())
-    }
-}
-
-struct UnitsEntry {
-    units: Vec<UnitEntry>,
-}
-
-impl UnitsEntry {
-    fn empty() -> Self {
-        Self { units: vec![] }
-    }
-}
-impl Parse for UnitsEntry {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let content;
-        let _: token::Bracket = bracketed!(content in input);
-        let units: Punctuated<UnitEntry, Token![,]> = content.parse_terminated(UnitEntry::parse)?;
-        Ok(UnitsEntry {
-            units: units.into_iter().collect(),
-        })
-    }
-}
-
-enum QuantityDefinitionEntry {
-    Dimensions(DimensionsEntry),
-    Units(UnitsEntry),
-}
-
-impl QuantityDefinitionEntry {
-    fn parse_named(input: ParseStream) -> Result<Self> {
-        let name: Ident = input.parse()?;
-        let _: Token![:] = input.parse()?;
-        match name.to_string().as_str() {
-            "dimension" => Ok(Self::Dimensions(input.parse()?)),
-            "units" => Ok(Self::Units(input.parse()?)),
-            ident => Err(Error::new(
-                ident.span(),
-                format!("Unexpected identifier: {}", ident),
-            )),
-        }
-    }
-
-    fn unwrap_dimensions(self) -> DimensionsEntry {
-        match self {
-            QuantityDefinitionEntry::Dimensions(dims) => dims,
-            QuantityDefinitionEntry::Units(_) => {
-                panic!("unwrap_dimensions called on Units variant")
-            }
-        }
-    }
-
-    fn unwrap_units(self) -> UnitsEntry {
-        match self {
-            QuantityDefinitionEntry::Units(units) => units,
-            QuantityDefinitionEntry::Dimensions(_) => {
-                panic!("unwrap_units called on Dimensions variant")
-            }
-        }
-    }
-}
-
-struct QuantityEntry {
-    name: Ident,
-    dimensions_def: DimensionsEntry,
-    units_def: UnitsEntry,
-}
-
-fn get_unique_entry<T>(
-    mut defs: Vec<QuantityDefinitionEntry>,
-    span: proc_macro2::Span,
-    name: &str,
-    unwrap: fn(QuantityDefinitionEntry) -> T,
-    allow_none: bool,
-) -> Result<Option<T>> {
-    if defs.len() == 0 {
-        if allow_none {
-            return Ok(None);
-        }
-        return Err(Error::new(
-            span,
-            format!("No definition of {} given for quantity.", name),
-        ));
-    }
-    if defs.len() > 1 {
-        return Err(Error::new(
-            span,
-            format!("More than one definition of {} given for quantity.", name),
-        ));
-    }
-    Ok(Some(unwrap(defs.remove(0))))
-}
-
-impl QuantityEntry {
-    pub fn parse_named(input: ParseStream) -> Result<Self> {
-        let name: Ident = input.parse()?;
-        let _: token::Eq = input.parse()?;
-        let content;
-        let _: token::Brace = braced!(content in input);
-        let quantities: Punctuated<_, Token![,]> =
-            content.parse_terminated(QuantityDefinitionEntry::parse_named)?;
-        let (dimensions_def, units_def): (Vec<_>, Vec<_>) = quantities
-            .into_iter()
-            .partition(|entry| matches!(entry, QuantityDefinitionEntry::Dimensions(..)));
-        let dimensions_def = get_unique_entry(
-            dimensions_def,
-            input.span(),
-            "dimensions",
-            QuantityDefinitionEntry::unwrap_dimensions,
-            false,
-        )?
-        .unwrap();
-        let units_def = get_unique_entry(
-            units_def,
-            input.span(),
-            "units",
-            QuantityDefinitionEntry::unwrap_units,
-            true,
-        )?
-        .unwrap_or(UnitsEntry::empty());
-        Ok(Self {
-            name,
-            dimensions_def,
-            units_def,
-        })
-    }
-}
-
-impl Parse for Defs {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let dimension_type: Type = input.parse()?;
-        let _: Token![,] = input.parse()?;
-        let quantity_type: Type = input.parse()?;
-        let _: Token![,] = input.parse()?;
-        let content;
-        let _: token::Bracket = bracketed!(content in input);
-        let quantities: Punctuated<_, Token![,]> =
-            content.parse_terminated(QuantityEntry::parse_named)?;
-        Ok(Self {
-            dimension_type,
-            quantity_type,
-            quantities: quantities.into_iter().collect(),
-        })
     }
 }
--- src/si/mod.rs
+++ src/si/mod.rs
@@ -7,8 +7,6 @@ use crate::define_system;
 use crate::unit_system;
 use ::unit_system::unit_system_2;
 
-define_system!(Quantity, Dimension, NONE, UNIT_NAMES);
-
 // #[rustfmt::skip]
 // unit_system!(
 //     Dimension,
@@ -92,19 +90,28 @@ define_system!(Quantity, Dimension, NONE, UNIT_NAMES);
 //     );
 
 unit_system_2!(
+    UNIT_NAMES,
     Dimension,
     Quantity,
     [
         Dimensionless = {
             dimension: {
-                length: 0,
-                time: 0,
-            }
+            },
+            units: [
+                { name: dimensionless, factor: 1.0, symbol: "dimensionless" }
+            ]
         },
         Length = {
             dimension: { length: 1 },
             units: [
-                { name: meters, factor: 1.0, symbol: "m", prefixes: [k, m] }
+                { name: meters, factor: 1.0, symbol: "m", prefixes: [k, m] },
+                { name: kilometers, factor: 1000.0, symbol: "km", prefixes: [k, m] }
+            ]
+        },
+        Time = {
+            dimension: { time: 1 },
+            units: [
+                { name: seconds, factor: 1.0, symbol: "s" }
             ]
         },
         Velocity = {
@@ -112,6 +119,32 @@ unit_system_2!(
             units: [
                 { name: meters_per_second, factor: 1.0, symbol: "m/s" }
             ],
-        }
+        },
+        Energy = {
+            dimension: { length: 2, time: -2, mass: 1 },
+            units: [
+                { name: joules, factor: 1.0, symbol: "J" },
+            ],
+        },
+        Mass = {
+            dimension: { mass: 1 },
+            units: [
+                { name: kilograms, factor: 1.0, symbol: "kg" },
+            ],
+        },
+        Area = {
+            dimension: { length: 2 },
+        },
+        Volume = {
+            dimension: { length: 3 },
+        },
+        Force = {
+            dimension: { length: 1, time: -2, mass: 1 },
+            units: [
+                { name: newtons, factor: 1.0, symbol: "N" }
+            ]
+        },
     ]
 );
+
+define_system!(Quantity, Dimension, NONE, UNIT_NAMES);
