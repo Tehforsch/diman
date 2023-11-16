4e3c907ee29040cfddfe84ba4c32c7540e06e9da
tehforsch <mail@tonipeter.de>
2023-11-16 20:00:19 +0100

    refactor tests to reduce boilerplate

--- tests/compile_fail/type_mismatch_mul_quantity_type.rs
+++ tests/compile_fail/type_mismatch_mul_quantity_type.rs
@@ -1,26 +1,7 @@
 #![allow(incomplete_features)]
-#![feature(adt_const_params)]
-#![feature(generic_const_exprs)]
-
-use diman::{dimension, unit_system};
-
-#[dimension]
-pub struct Dimension {
-    time: i32,
-    length: i32,
-}
-
-#[rustfmt::skip]
-unit_system!(
-    Quantity,
-    Dimension,
-    [
-        def Dimensionless = {},
-        unit dimensionless = Dimensionless,
-    ]
-);
-
-use crate::f64::*;
+#![feature(generic_const_exprs, adt_const_params)]
+pub mod example_system;
+use example_system::f64::*;
 
 fn main() {
     let x: () = 1.0 * Dimensionless::dimensionless(1.0);
