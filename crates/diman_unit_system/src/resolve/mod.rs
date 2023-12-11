ce53336f169ae4b80697ef736e90d4a10cd1ffbe
tehforsch <mail@tonipeter.de>
2023-12-11 22:29:20 +0100

    fix default quantity type

--- crates/diman_unit_system/src/resolve/mod.rs
+++ crates/diman_unit_system/src/resolve/mod.rs
@@ -21,6 +21,10 @@ fn default_dimension_type() -> Ident {
     Ident::new("Dimension", Span::call_site())
 }
 
+fn default_quantity_type() -> Ident {
+    Ident::new("quantity", Span::call_site())
+}
+
 /// A helper function for emitting all the errors contained in the
 /// result types but continuing with a partial result anyways This is
 /// done so that we at least define all the quantities that can be
@@ -35,8 +39,16 @@ fn emit_errors<T, E: Emit>((input, result): (T, std::result::Result<(), E>)) -> 
 
 impl UnresolvedDefs {
     pub fn resolve(self) -> Defs {
-        let quantity_type = emit_errors(get_single_ident(self.quantity_types, "quantity type"));
-        let dimension_type = emit_errors(get_single_ident(self.dimension_types, "dimension type"));
+        let quantity_type = emit_errors(get_single_ident(
+            self.quantity_types,
+            "quantity type",
+            default_quantity_type,
+        ));
+        let dimension_type = emit_errors(get_single_ident(
+            self.dimension_types,
+            "dimension type",
+            default_dimension_type,
+        ));
         let base_dimensions = self
             .dimensions
             .iter()
@@ -70,11 +82,12 @@ impl UnresolvedDefs {
 fn get_single_ident(
     mut dimension_types: Vec<Ident>,
     type_name: &'static str,
+    default: impl Fn() -> Ident,
 ) -> (Ident, std::result::Result<(), MultipleTypeDefinitionsError>) {
     if dimension_types.len() == 1 {
         (dimension_types.remove(0), Ok(()))
     } else if dimension_types.is_empty() {
-        (default_dimension_type(), Ok(()))
+        (default(), Ok(()))
     } else {
         let dimension_type = dimension_types[0].clone();
         (
