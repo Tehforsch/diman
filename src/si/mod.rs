aa2ee3add456c7bf4aceda8313036e3c9aea8316
tehforsch <mail@tonipeter.de>
2023-12-29 17:40:49 +0100

    improve si module

--- src/si/mod.rs
+++ src/si/mod.rs
@@ -90,11 +90,14 @@ unit_system!(
 
     dimension KinematicViscosity = Length^2 / Time;
 
+    dimension MomentumDensity = Momentum / Volume;
+    dimension SpecificEnergy = Energy / Mass;
+    dimension MassFlux = Mass / (Area * Time);
+    dimension EnergyFlux = Power / Area;
+
     #[metric_prefixes]
     #[symbol(m)]
-    #[alias(metre)]
-    #[alias(metres)]
-    #[alias(meters)]
+    #[alias(metre, metres, meters)]
     #[base(Length)]
     unit meter: Length;
 
@@ -108,7 +111,7 @@ unit_system!(
     #[alias(kilograms)]
     unit kilogram: Mass;
 
-    #[metric_prefixes]
+    #[metric_prefixes(skip: kilo)]
     #[symbol(g)]
     #[alias(grams)]
     unit gram: Mass = 1.0e-3 * kilogram;
@@ -295,53 +298,21 @@ unit_system!(
     #[alias(electronvolts)]
     unit electronvolt: Energy = 1.602_176_634e-19 joule;
 
+    #[metric_prefixes]
+    #[alias(joules_per_kilogram)]
+    unit joule_per_kilogram: SpecificEnergy = joule / kilogram;
 
-    // #[base(Dimensionless)]
-    // unit dimensionless: Dimensionless;
-    // #[base(Length)]
-    // #[metric_prefixes]
-    // unit meters: Length;
-    // #[base(Time)]
-    // #[metric_prefixes]
-    // unit seconds: Time;
-    // unit hours: Time = 3600 * seconds;
-    // dimension Velocity = Length / Time;
-    // unit meters_per_second = meters / seconds;
-    // dimension Area = Length * Length;
-    // unit square_meters = meters^2;
-    // dimension Volume = Length^3;
-    // unit cubic_meters = meters^3;
-    // #[base(Mass)]
-    // unit kilograms: Mass;
-    // unit grams = 1e-3 * kilograms;
-    // dimension MassDensity = Mass / Volume;
-    // unit kilograms_per_cubic_meter = kilograms / cubic_meters;
-    // dimension Momentum = Mass * Velocity;
-    // unit kilograms_meter_per_second = kilograms / square_meters;
-    // dimension MomentumDensity = Momentum / Volume;
-    // unit kilograms_per_meter_squared_second = kilograms / (square_meters * seconds);
-    // dimension Force = Mass * Velocity / Time;
-    // unit newtons = kilograms * meters / (seconds^2);
-    // dimension Pressure = Force / Area;
-    // unit pascals: Pressure = newtons / square_meters;
-    // #[base(Temperature)]
-    // unit kelvin: Temperature;
-    // dimension Energy = Force * Length;
-    // unit joules = newtons * meters;
-    // dimension SpecificEnergy = Energy / Mass;
-    // unit joules_per_kilogram = joules / kilograms;
-    // dimension SpecificHeatCapacity = SpecificEnergy / Temperature;
-    // unit joules_per_kilogram_kelvin = joules_per_kilogram / kelvin;
-    // dimension EnergyDensity = Energy / Volume;
-    // unit joules_per_cubic_meter = joules / cubic_meters;
-    // dimension Power = Energy / Time;
-    // unit watts = joules / seconds;
-    // dimension MassFlux = Mass / (Area * Time);
-    // unit kilograms_per_square_meter_second = kilograms / square_meters / seconds;
-    // dimension EnergyFlux = Power / Area;
-    // unit watts_per_square_meter = watts / square_meters;
-    // dimension DynamicViscosity = Pressure / Time;
-    // unit pascal_seconds = pascals * seconds;
-    // dimension KinematicViscosity = Area / Time;
-    // unit square_meters_per_second = square_meters / seconds;
+    #[metric_prefixes]
+    #[alias(joules_per_kilogram_kelvin)]
+    unit joule_per_kilogram_kelvin: SpecificHeatCapacity  = joule_per_kilogram / kelvin;
+
+    #[metric_prefixes]
+    #[alias(meters_per_second)]
+    unit meter_per_second: Velocity = meter / second;
+
+    #[alias(square_metre, square_meters, square_metres)]
+    unit square_meter: Area = meter^2;
+
+    #[alias(cubic_metre, cubic_meters, cubic_metres)]
+    unit cubic_meter: Volume = meter^3;
 );
