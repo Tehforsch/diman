03d6e9dcc66f46642266c319d48b43799f71aef6
tehforsch <mail@tonipeter.de>
2024-01-12 16:00:53 +0100

    activate compile_fail tests only without any of the external dependency features, make sure diman_unit_system itself is tested with default features

--- crates/diman_unit_system/tests/mod.rs
+++ crates/diman_unit_system/tests/mod.rs
@@ -1,33 +1,47 @@
-#[test]
-#[cfg(feature = "f64")]
-fn compile_fail_float() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/compile_fail/float_*.rs");
-}
+// Having these features on means the generated code will use them
+// which in turns requires us to have the crates as dev-dependencies.
+// None of these tests depend any of the functionality, so I'd rather
+// just not run the tests in the case any of them are activated.
+// Tests are run with default-features enabled, which is in CI.
+#[cfg(not(any(
+    feature = "serde",
+    feature = "glam",
+    feature = "mpi",
+    feature = "hdf5",
+    feature = "rand",
+)))]
+mod tests {
+    #[test]
+    #[cfg(feature = "f64")]
+    fn compile_fail_float() {
+        let t = trybuild::TestCases::new();
+        t.compile_fail("tests/compile_fail/float_*.rs");
+    }
 
-#[test]
-#[cfg(feature = "f64")]
-fn compile_fail_resolver() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/compile_fail/resolver_*.rs");
-}
+    #[test]
+    #[cfg(feature = "f64")]
+    fn compile_fail_resolver() {
+        let t = trybuild::TestCases::new();
+        t.compile_fail("tests/compile_fail/resolver_*.rs");
+    }
 
-#[test]
-#[cfg(feature = "f64")]
-#[cfg(not(feature = "rational-dimensions"))]
-fn compile_fail_type_mismatch() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/compile_fail/type_mismatch_*.rs");
-}
+    #[test]
+    #[cfg(feature = "f64")]
+    #[cfg(not(feature = "rational-dimensions"))]
+    fn compile_fail_type_mismatch() {
+        let t = trybuild::TestCases::new();
+        t.compile_fail("tests/compile_fail/type_mismatch_*.rs");
+    }
 
-#[test]
-fn compile_fail_dimension_def_numeric_factor() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/compile_fail/dimension_definition_with_numeric_factor.rs");
-}
+    #[test]
+    fn compile_fail_dimension_def_numeric_factor() {
+        let t = trybuild::TestCases::new();
+        t.compile_fail("tests/compile_fail/dimension_definition_with_numeric_factor.rs");
+    }
 
-#[test]
-fn compile_fail_dimension_annotation() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/compile_fail/dimension_annotation*.rs");
+    #[test]
+    fn compile_fail_dimension_annotation() {
+        let t = trybuild::TestCases::new();
+        t.compile_fail("tests/compile_fail/dimension_annotation*.rs");
+    }
 }
