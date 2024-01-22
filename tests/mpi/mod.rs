b56c8d257c8942a09c7c70c53afec57b1d549991
tehforsch <mail@tonipeter.de>
2024-01-22 18:49:34 +0100

    revive mpi tests

--- tests/mpi/mod.rs
+++ tests/mpi/mod.rs
@@ -13,14 +13,14 @@ lazy_static::lazy_static! {
 macro_rules! gen_tests_for_float {
     ($float_name: ident) => {
         mod $float_name {
-            use crate::example_system::$float_name::Length;
+            use crate::example_system::units::meters;
             use mpi::traits::Communicator;
 
             #[test]
-            fn pack_unpack_f64_quantity() {
+            fn pack_unpack_float_quantity() {
                 let world = super::MPI_UNIVERSE.world();
-                let q1 = Length::meters(1.0);
-                let mut q2 = Length::meters(2.0);
+                let q1 = 1.0 as $float_name * meters;
+                let mut q2 = 2.0 as $float_name * meters;
                 let a = world.pack(&q1);
                 unsafe {
                     world.unpack_into(&a, &mut q2, 0);
@@ -33,16 +33,17 @@ macro_rules! gen_tests_for_float {
 
 #[cfg(any(feature = "glam-vec2", feature = "glam-dvec2"))]
 macro_rules! gen_tests_for_vector_2 {
-    ($vec_mod_name: ident) => {
+    ($vec_mod_name: ident, $vec_name: ident) => {
         mod $vec_mod_name {
-            use crate::example_system::$vec_mod_name::Length as VecLength;
+            use crate::example_system::units::meters;
+            use glam::$vec_name;
             use mpi::topology::Communicator;
 
             #[test]
             fn pack_unpack_vec_quantity() {
                 let world = super::MPI_UNIVERSE.world();
-                let q1 = VecLength::meters(1.0, 2.0);
-                let mut q2 = VecLength::meters(3.0, 4.0);
+                let q1 = <$vec_name>::new(1.0, 2.0) * meters;
+                let mut q2 = <$vec_name>::new(3.0, 4.0) * meters;
                 let a = world.pack(&q1);
                 unsafe {
                     world.unpack_into(&a, &mut q2, 0);
@@ -55,16 +56,17 @@ macro_rules! gen_tests_for_vector_2 {
 
 #[cfg(any(feature = "glam-vec3", feature = "glam-dvec3"))]
 macro_rules! gen_tests_for_vector_3 {
-    ($vec_mod_name: ident) => {
+    ($vec_mod_name: ident, $vec_name: ident) => {
         mod $vec_mod_name {
-            use crate::example_system::$vec_mod_name::Length as VecLength;
+            use crate::example_system::units::meters;
+            use glam::$vec_name;
             use mpi::topology::Communicator;
 
             #[test]
             fn pack_unpack_vec_quantity() {
                 let world = super::MPI_UNIVERSE.world();
-                let q1 = VecLength::meters(1.0, 2.0, 3.0);
-                let mut q2 = VecLength::meters(3.0, 4.0, 5.0);
+                let q1 = <$vec_name>::new(1.0, 2.0, 3.0) * meters;
+                let mut q2 = <$vec_name>::new(4.0, 5.0, 6.0) * meters;
                 let a = world.pack(&q1);
                 unsafe {
                     world.unpack_into(&a, &mut q2, 0);
@@ -82,13 +84,13 @@ gen_tests_for_float!(f32);
 gen_tests_for_float!(f64);
 
 #[cfg(all(feature = "f32", feature = "glam-vec2"))]
-gen_tests_for_vector_2!(vec2);
+gen_tests_for_vector_2!(vec2, Vec2);
 
 #[cfg(all(feature = "f64", feature = "glam-dvec2"))]
-gen_tests_for_vector_2!(dvec2);
+gen_tests_for_vector_2!(dvec2, DVec2);
 
 #[cfg(all(feature = "f32", feature = "glam-vec3"))]
-gen_tests_for_vector_3!(vec3);
+gen_tests_for_vector_3!(vec3, Vec3);
 
 #[cfg(all(feature = "f64", feature = "glam-dvec3"))]
-gen_tests_for_vector_3!(dvec3);
+gen_tests_for_vector_3!(dvec3, DVec3);
